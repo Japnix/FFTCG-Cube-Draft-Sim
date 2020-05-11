2e5598ffc7d9a076cac5d8d900f4ba540bb777c1
btawa <brendan.tawa@gmail.com>
2020-05-11 10:48:03 -0400

    Adding some adding ability to handle dynamic input of players

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import requests
 import random
 
-CUBE_URL = 'http://dev.tawa.wtf:8000/api/cube/?api_key=lolbbq'
-PLAYERS = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8']
-CARDS_PER_PACK = 12
-PACKS_PER_PLAYER = 5
-TOTAL_PACKS = len(PLAYERS) * PACKS_PER_PLAYER
 DEBUG = False
 
 
 class Cube:
-    def __init__(self):
-        self.cards = requests.get(CUBE_URL).json()
+    def __init__(self, cardsperpack, totalpacks):
+        self.cubeurl = 'http://dev.tawa.wtf:8000/api/cube/?api_key=lolbbq'
+        self.cards = requests.get(self.cubeurl).json()
         self.packs = []
+        self.cardsperpack = cardsperpack
+        self.totalpacks = totalpacks
 
     def getCards(self):
         return self.cards
@@ -22,15 +20,15 @@ class Cube:
 
     def makePack(self):
         pack = Pack()
-        if len(self.cards) >= CARDS_PER_PACK:
-            for x in range(0, CARDS_PER_PACK):
+        if len(self.cards) >= self.cardsperpack:
+            for x in range(0, self.cardsperpack):
                 pack.addCard(self.cards[0])
                 self.cards.pop(0)
 
         return pack
 
     def makeAllPacks(self):
-        for x in range(0,TOTAL_PACKS):
+        for x in range(0,self.totalpacks):
             self.packs.append(self.makePack())
 
     def getAllPacks(self):
@@ -90,31 +88,49 @@ class Player:
 
 
 class Game:
-    def __init__(self, cube, players):
+    def __init__(self, numofplayers):
         self.players = []
+        self.numofplayers = numofplayers
         self.round = 1
-        self.card = 1
-
-        for name in players:
-            player = Player(name)
-            self.addPlayer(player)
-
-        self.cube = cube
+        self.packsperplayer = 5
+        self.cardsperpack = 12
+        self.totalpacks = self.packsperplayer * self.numofplayers
+        self.cube = Cube(cardsperpack=self.cardsperpack, totalpacks=self.totalpacks)
 
     def addPlayer(self, player):
         self.players.append(player)
 
     def giveAllPacks(self):
         for player in self.players:
-            for x in range(0, PACKS_PER_PLAYER):
+            for x in range(0, self.packsperplayer):
                 player.addPack(self.cube.packs[0])
                 self.cube.packs.pop(0)
 
+    def askNumPlayers(self):
+        while True:
+            try:
+                numberofplayers = int(input('How many players will be drafting (1-8)?'))
+                while numberofplayers not in range(1,9):
+                    print('This is not a valid number of players')
+                    numberofplayers = int(input('How many players will be drafting (1-8)?'))
+
+                return numberofplayers
+            except:
+                print('You did not input an integer')
+
+    def askPlayerNames(self):
+        for i in range(0,self.numofplayers):
+            name = input(f"What is Player {i+1}'s Name?")
+            player = Player(name)
+            self.addPlayer(player)
+
     def prepareGame(self):
+        self.numofplayers = self.askNumPlayers()
+        self.askPlayerNames()
         self.cube.shuffleCube()
         self.cube.makeAllPacks()
         self.giveAllPacks()
-        
+
     def selectionRound(self, player, pack):
 
         def askForChoice(pack):
@@ -150,8 +166,8 @@ class Game:
     def allPassLeft(self):
         for player in self.players:
             index = self.getPlayerIndex(player)
-            player.queuedpack = game.players[(index + 1) % 8].currentpack
-            print(f"{player.name} gets {game.players[(index + 1) % 8].name}'s pack ")
+            player.queuedpack = game.players[(index + 1) % self.numofplayers].currentpack
+            print(f"{player.name} gets {game.players[(index + 1) % self.numofplayers].name}'s pack ")
 
         for player in self.players:
             player.currentpack = player.queuedpack
@@ -159,8 +175,8 @@ class Game:
     def allPassRight(self):
         for player in self.players:
             index = self.getPlayerIndex(player)
-            player.queuedpack = game.players[(index - 1) % 8].currentpack
-            print(f"{player.name} gets {game.players[(index - 1) % 8].name}'s pack ")
+            player.queuedpack = game.players[(index - 1) % self.numofplayers].currentpack
+            print(f"{player.name} gets {game.players[(index - 1) % self.numofplayers].name}'s pack ")
 
         for player in self.players:
             player.currentpack = player.queuedpack
@@ -193,7 +209,7 @@ class Game:
         print('Finished Game')
 
 
-game = Game(cube=Cube(), players=PLAYERS)
+game = Game(numofplayers=8)
 game.prepareGame()
 game.startGame()
 
